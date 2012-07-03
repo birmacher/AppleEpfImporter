d8491b6927c4e5cd61f80f15763643a8fb1fdf9e
Barnabas Birmacher <birmacher@gmail.com>
2012-07-03 14:33:31 +0200

    download increment by date

--- lib/apple_epf_importer/apple_epf_downloader.rb
+++ lib/apple_epf_importer/apple_epf_downloader.rb
@@ -11,17 +11,12 @@ module AppleEpfImporter
       start_download( url, [AppleEpfImporter.configuration.extract_dir, File.basename( url_path )].join('/') )
     end
   
-    # TODO: not finished
     def get_date_file_name(type, filedate)
       today = DateTime.now
       case type
       when 'full'
        if filedate == 'current'
-         days_from_wed = today.wday == 3 ? 0 : 3 - today.wday
-         day_diff = days_from_wed > 0 ? days_from_wed - 7 : days_from_wed
-         date_of_file = today + day_diff
-         
-         date = date_of_file.strftime('%Y%m%d')
+         date = main_dir_name_by_date(today)
          "current/itunes#{date}.tbz"
        else
          date = filedate.strftime('%Y%m%d')
@@ -32,13 +27,22 @@ module AppleEpfImporter
          date = today.strftime('%Y%m%d')
          "current/incremental/current/itunes#{date}.tbz"
        else
-          # TODO: implement
-          ""
+          main_date = main_dir_name_by_date(today)
+          date = filedate.strftime('%Y%m%d')
+          "#{main_date}/incremental/#{date}/itunes#{date}.tbz"
        end 
       end 
     end 
   
     private
+    
+    def main_dir_name_by_date(date)
+      days_from_wed = date.wday == 3 ? 0 : 3 - date.wday
+      day_diff = days_from_wed > 0 ? days_from_wed - 7 : days_from_wed
+      date_of_file = date + day_diff
+         
+      date_of_file.strftime('%Y%m%d')
+    end
   
     def start_download(url, filename)
       # http://stackoverflow.com/questions/8196325/encoding-error-when-saving-a-document-through-a-rake-task-on-rails
